20eac9c3890e7ef92477f67a581b72dc19ec05dd
Anthony Cerbic <kuzzdra@gmail.com>
2018-12-28 19:20:09 +0300

    improve docker-compose

--- full_build.cmd
+++ full_build.cmd
@@ -2,13 +2,5 @@
 cd gamesdb
 CALL npm version patch
 CALL npm publish --access public
-CALL docker build -t deployer .
 
-cd ../gmaster
-CALL docker build -t gmaster .
-
-cd ../ghost
-CALL docker build -t ghost .
-
-cd ..
-CALL docker-compose up -d
+CALL docker-compose build
--- gmaster/app.js
+++ gmaster/app.js
@@ -1,4 +1,4 @@
-var express = require('express');
+const express = require('express');
 var morgan = require('morgan');
 
 var app = express();
