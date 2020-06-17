5fb61f3a0f86951ddb8607570179bfdd82505305
Antony Cerbic <kuzzdra@gmail.com>
2020-06-17 10:02:24 +0300

    fix docker login

--- deployment/travis_build_push.sh
+++ deployment/travis_build_push.sh
@@ -9,7 +9,7 @@ docker tag ttt/ghost-prod $DOCKER_REGISTRY_ADDR/ttt/ghost
 docker tag ttt/gmaster-prod $DOCKER_REGISTRY_ADDR/ttt/gmaster
 
 # push containers to the remote private Docker repo
-echo "$DOCKER_REGISTRY_PASS" | docker login -u "travisci" --password-stdin
+echo "$DOCKER_REGISTRY_PASS" | docker login -u "travisci" --password-stdin $DOCKER_REGISTRY_ADDR
 docker push $DOCKER_REGISTRY_ADDR/ttt/hasura
 docker push $DOCKER_REGISTRY_ADDR/ttt/ghost
 docker push $DOCKER_REGISTRY_ADDR/ttt/gmaster
