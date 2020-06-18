d0c2b0dcb0c5f8ab2c0d22541a64c3c4ddfe92ff
Antony Cerbic <kuzzdra@gmail.com>
2020-06-18 23:15:00 +0300

    add Zeit deploy script to Travis

--- deployment/travis_deploy_client.sh
+++ deployment/travis_deploy_client.sh
@@ -3,4 +3,14 @@
 # Should be run from the root of the project
 # triggers build and deploy on Zeit Now (uses .nowignore and now.json to
 # configure what is to upload and what is to build)
-now -b GHOST_URL=$GHOST_URL 
+
+if [ "$TRAVIS_PULL_REQUEST" = "false" ] AND [ "$TRAVIS_BRANCH" = "master" ]; then 
+
+    # Unfortunately, we have to use the stupid "Project Linking" feature, hence the
+    # '--confirm' arg. Need 'name' key in `now.json` file, but since it was declared
+    # deprecated and no alternative provided, this deploy flow can break at any time
+    # in the future.
+    now -b GHOST_URL=$GHOST_URL --token $ZEIT_NOW_TOKEN --confirm --prod
+else
+    now -b GHOST_URL=$GHOST_URL --token $ZEIT_NOW_TOKEN --confirm
+fi
