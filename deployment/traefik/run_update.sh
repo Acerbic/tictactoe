8441b19845325b23a5b6a964003b38543b856f61
Antony Cerbic <kuzzdra@gmail.com>
2020-09-01 23:09:14 +0300

    check docker traefik network before creating

--- deployment/traefik/run_update.sh
+++ deployment/traefik/run_update.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # create traefik custom network if not existing already
-# TODO: check if exists first
-docker network create traefik_proxy
+# check if exists first
+EXISTING=$(docker network ls | grep "traefik_proxy")
+if [ "$EXISTING" = "" ]; then
+    docker network create traefik_proxy;
+fi
 
 # updates running containers to the newest version from docker registry
 docker-compose pull
