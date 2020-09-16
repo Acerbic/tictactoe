3b62629da0f467ebf165344d9b204b3378ec7f31
Antony Cerbic <kuzzdra@gmail.com>
2020-09-17 02:41:00 +0300

    [ci skip] cleaning up

--- deployment/travis_deploy_docker.sh
+++ deployment/travis_deploy_docker.sh
@@ -9,15 +9,5 @@ docker push $DOCKER_REGISTRY_ADDR/ttt/hasura
 docker push $DOCKER_REGISTRY_ADDR/ttt/ghost
 docker push $DOCKER_REGISTRY_ADDR/ttt/gmaster
 
-# # update container startup files on remote
-# ssh -o StrictHostKeyChecking=no $DOCKER_DEPLOY_USER@$DOCKER_DEPLOY_HOST "cd $DOCKER_DEPLOY_PATH; rm *"
-# rsync -r deployment/traefik/ $DOCKER_DEPLOY_USER@$DOCKER_DEPLOY_HOST:$DOCKER_DEPLOY_PATH
-
-# # generate configuration for Traefik / Docker Compose
-# echo "JWT_SECRET=$JWT_SECRET" > deployment/traefik/.env
-# echo "DOCKER_REGISTRY=$DOCKER_REGISTRY_ADDR" >> deployment/traefik/.env
-# echo "HOST_BASE_DOMAIN=$TRAEFIK_DOMAIN_BASE" >> deployment/traefik/.env
-# rsync deployment/traefik/.env $DOCKER_DEPLOY_USER@$DOCKER_DEPLOY_HOST:$DOCKER_DEPLOY_PATH/.env
-
 # Run update script to replace running containers with new pushed images
 ssh $DOCKER_DEPLOY_USER@$DOCKER_DEPLOY_HOST "cd $DOCKER_DEPLOY_PATH; sudo /bin/bash $DOCKER_DEPLOY_PATH/run_update.sh"
