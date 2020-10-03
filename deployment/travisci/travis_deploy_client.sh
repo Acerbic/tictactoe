2e8b0e7c53dec603f70b7c51d2cfbd39984ae48e
Antony Cerbic <kuzzdra@gmail.com>
2020-10-03 22:35:51 +0300

    deploy: now->vercel

--- deployment/travisci/travis_deploy_client.sh
+++ deployment/travisci/travis_deploy_client.sh
@@ -8,13 +8,13 @@ set -ev
 if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ] ;
 then 
 
-    # Unfortunately, we have to use the stupid "Project Linking" feature, hence the
-    # '--confirm' arg. Need 'name' key in `now.json` file, but since it was declared
-    # deprecated and no alternative provided, this deploy flow can break at any time
-    # in the future.
-    echo "Now: deploying to production"
-    now --token $ZEIT_NOW_TOKEN --confirm --prod
+    # Unfortunately, we have to use the stupid "Project Linking" feature, hence
+    # the '--confirm' arg. Need 'name' key in `vercel.json` file, but since it
+    # was declared deprecated and no alternative provided (defaults to the
+    # directory name), this deploy flow can break at any time in the future.
+    echo "Vercel: deploying to production"
+    vercel --token $ZEIT_NOW_TOKEN --confirm --prod
 else
-    echo "Now: deploying to staging"
-    now --token $ZEIT_NOW_TOKEN --confirm
+    echo "Vercel: deploying to staging"
+    vercel --token $ZEIT_NOW_TOKEN --confirm
 fi
