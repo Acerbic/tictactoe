dbe56f0c52b255bcf6431ddcf92ea8cf9f073e33
Antony Cerbic <kuzzdra@gmail.com>
2020-11-15 21:27:01 +0300

    (ghost) exclude "dist" from testing

--- packages/ghost/jest.config.js
+++ packages/ghost/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
-    testPathIgnorePatterns: ["__tests__/.*__.*\\.ts", "/node_modules/"],
+    testPathIgnorePatterns: ["__tests__/.*__.*\\.ts", "/node_modules/", "dist"],
     testTimeout: process.env.VSCODE_CLI ? 1000000 : 5000
 };
